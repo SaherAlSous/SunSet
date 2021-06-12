db25396f0c3c42b01dfec102cc74f160750ef528
Saher AlSous <saher.alsous@gmail.com>
2021-06-12 20:13:02 +0300

    Chapter 31 - Challenges - Done

--- app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -3,12 +3,17 @@ package com.bignerdranch.android.sunset
 import android.animation.AnimatorSet
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.AnimationSet
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import kotlin.time.times
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,7 +31,18 @@ class MainActivity : AppCompatActivity() {
         ContextCompat.getColor(this, R.color.night_sky)
     }
 
+    private val heatSun : Int by lazy {
+        ContextCompat.getColor(this, R.color.hot_sun)
+    }
 
+    private val normalSun : Int by lazy {
+        ContextCompat.getColor(this, R.color.bright_sun)
+    }
+
+        var animated = false
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,12 +51,14 @@ class MainActivity : AppCompatActivity() {
         sunView = findViewById(R.id.sun)
         skyView = findViewById(R.id.sky)
 
+
         sceneView.setOnClickListener{
-            startAnimation()
+            if (!animated) startAnimation() else reverse()
         }
 
     }
 
+    @SuppressLint("ObjectAnimatorBinding")
     private fun startAnimation(){ //page. 626
         val sunYStart = sunView.top.toFloat()
         val sunYEnd = skyView.height.toFloat()
@@ -48,6 +66,12 @@ class MainActivity : AppCompatActivity() {
         val heightAnimator = ObjectAnimator
             .ofFloat(sunView, "y", sunYStart, sunYEnd)
             .setDuration(3000)
+
+        val sunHeat = ObjectAnimator
+            .ofInt(sunView, "Color",normalSun, heatSun)
+            .setDuration(1000)
+
+
 
         val sunSetSkyAnimator = ObjectAnimator
             .ofInt(skyView, "backgroundColor", blueSkyColor, sunSetSkyColor)
@@ -64,10 +88,48 @@ class MainActivity : AppCompatActivity() {
 
         val animatorSet = AnimatorSet()
         animatorSet.play(heightAnimator)
+            .with(sunHeat)
             .with(sunSetSkyAnimator)
             .before(nightSkyAnimator)
         animatorSet.start()
+        animated = true
+     }
 
+    @SuppressLint("ObjectAnimatorBinding")
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun reverse(){
+        val sunYStart = sunView.top.toFloat()
+        val sunYEnd = skyView.height.toFloat()
+
+        val heightAnimator = ObjectAnimator
+            .ofFloat(sunView, "y", sunYEnd,sunYStart)
+            .setDuration(3000)
+
+        val sunHeat = ObjectAnimator
+            .ofInt(sunView, "color",normalSun ,heatSun)
+
+        val sunSetSkyAnimator = ObjectAnimator
+            .ofInt(skyView, "backgroundColor", sunSetSkyColor, blueSkyColor)
+            .setDuration(3000)
+
+        val nightSkyAnimator =ObjectAnimator
+            .ofInt(skyView,"backgroundColor", nightSkyColor, sunSetSkyColor)
+            .setDuration(1500)
+
+
+        heightAnimator.interpolator = AccelerateInterpolator() //page. 631
+        sunSetSkyAnimator.setEvaluator(ArgbEvaluator()) //page. 633
+        nightSkyAnimator.setEvaluator(ArgbEvaluator()) //page. 634
+
+        val animatorSet = AnimatorSet()
+        animatorSet.play(heightAnimator)
+            .with(sunSetSkyAnimator)
+            .with(sunHeat)
+            .after(nightSkyAnimator)
+
+
+        animatorSet.start()
+        animated = false
     }
 
 }
